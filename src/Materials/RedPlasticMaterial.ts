6a15d1c7f66c61d3058e68d4d1dbb03003a004be
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2024-05-27 22:41:26 +0200

    Added parts for phase 4

--- src/Materials/RedPlasticMaterial.ts
+++ src/Materials/RedPlasticMaterial.ts
@@ -1,8 +1,8 @@
 import { Color, MeshPhysicalMaterial } from "three";
 
 export const redPlasticMaterial = new MeshPhysicalMaterial();
-redPlasticMaterial.roughness = 0.1;
-redPlasticMaterial.metalness = 0.8;
+redPlasticMaterial.roughness = 0.2;
+redPlasticMaterial.metalness = 0.6;
 redPlasticMaterial.color = new Color(0xf8191a);
 redPlasticMaterial.specularIntensity = 0.5;
 // redPlasticMaterial.clearcoat = 1;
--- src/PartsLists/steamLocomotivePartsList.ts
+++ src/PartsLists/steamLocomotivePartsList.ts
@@ -8,6 +8,9 @@ import plate_3023 from "../assets/3D/Plate_3023/Plate_3023.glb";
 import electricEngine_x469b from "../assets/3D/ElectricEngine_x469b/ElectricEngine_x469b.glb";
 import magnetCylindrical_73092 from "../assets/3D/MagnetCylindrical_73092/MagnetCylindrical_73092.glb";
 import trainCoupling_4023 from "../assets/3D/TrainCoupling_4023/TrainCoupling_4023.glb";
+import slope_3040 from "../assets/3D/Slope_3040/Slope_3040.glb";
+import plate_With_Ladder_4175 from "../assets/3D/4175_Plate_With_Ladder/4175_Plate_With_Ladder.glb";
+import brick_3003 from "../assets/3D/3003_Brick/3003_Brick.glb";
 
 import { PartInfo } from "../Types/PartInfo";
 
@@ -127,6 +130,57 @@ export const steamLocomotive7722Parts: PhaseParts = {
       partStartPosition: new Vector3(125, 0, 85),
       partId: "73092",
       partPriority: 3,
+    },
+  ],
+  phase4: [
+    {
+      partPath: slope_3040,
+      partStartPosition: new Vector3(165, 0, 85),
+      partId: "3040",
+      partPriority: 1,
+      materialId: "blackPlasticMaterial",
+    },
+    {
+      partPath: slope_3040,
+      partStartPosition: new Vector3(145, 0, 85),
+      partId: "3040",
+      partPriority: 1,
+      materialId: "blackPlasticMaterial",
+    },
+    {
+      partPath: plate_With_Ladder_4175,
+      partStartPosition: new Vector3(40, 0, 130),
+      partId: "4175",
+      partPriority: 1,
+      materialId: "redPlasticMaterial",
+    },
+    {
+      partPath: plate_With_Ladder_4175,
+      partStartPosition: new Vector3(65, 0, 130),
+      partId: "4175",
+      partPriority: 1,
+      materialId: "redPlasticMaterial",
+    },
+    {
+      partPath: brick_3003,
+      partStartPosition: new Vector3(145, 0, 60),
+      partId: "3003",
+      partPriority: 1,
+      materialId: "blackPlasticMaterial",
+    },
+    {
+      partPath: plate_3023,
+      partStartPosition: new Vector3(100, 0, 115),
+      partId: "3023",
+      partPriority: 1,
+      materialId: "blackPlasticMaterial",
+    },
+    {
+      partPath: plate_3023,
+      partStartPosition: new Vector3(120, 0, 115),
+      partId: "3023",
+      partPriority: 1,
+      materialId: "blackPlasticMaterial",
     },
   ],
 };
