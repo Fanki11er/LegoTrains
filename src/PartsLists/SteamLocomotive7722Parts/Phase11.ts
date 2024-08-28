0f9e0e8c4334d159b240a3da5b6a1f3a23ebbc1b
Krzysztof Dziedzic <fanki11er@hotmail.com>
2024-08-28 23:05:41 +0200

    Finished building of 7722 steam locomotive Fixed problem with parts in phase 7 Added missing wheels to phase 12 Added model markers for phase 12

--- src/PartsLists/SteamLocomotive7722Parts/Phase11.ts
+++ src/PartsLists/SteamLocomotive7722Parts/Phase11.ts
@@ -6,7 +6,7 @@ import { PartInfo } from "../../Types/PartInfo";
 import plate_1x6_3666 from "../../assets/3D/Plate_1x6_3666/plate_1x6_3666.glb";
 import brick_3003 from "../../assets/3D/3003_Brick/3003_Brick.glb";
 import round_Plate_4073 from "../../assets/3D/Round_Plate_4073/Round_Plate_4073.glb";
-import plate_1x4_3010 from "../../assets/3D/Plate_1x4_3010/Plate_1x4_3010.glb";
+import plate_1x4_3710 from "../../assets/3D/Plate_1x4_3710/Plate_1x4_3710.glb";
 import minifig_Head_Plain_Undecorated_3626b from "../../assets/3D/Minifig_Head_Plain_Undecorated_3626b/Minifig_Head_Plain_Undecorated_3626b.glb";
 
 export const phase11: PartInfo[] = [
@@ -46,9 +46,9 @@ export const phase11: PartInfo[] = [
     materialId: "redPlasticMaterial",
   },
   {
-    partPath: plate_1x4_3010,
+    partPath: plate_1x4_3710,
     partStartPosition: new Vector3(-180, 0, 135),
-    partId: "3010",
+    partId: "3710",
     partPriority: 1,
     materialId: "blackPlasticMaterial",
   },
--- src/PartsLists/SteamLocomotive7722Parts/Phase12.ts
+++ src/PartsLists/SteamLocomotive7722Parts/Phase12.ts
@@ -8,6 +8,7 @@ import cone_1x1_4589 from "../../assets/3D/Cone_1x1_4589/Cone_1x1_4589.glb";
 import train_Roof_6x6_4509 from "../../assets/3D/Train_Roof_6x6_4509/Train_Roof_6x6_4509.glb";
 import roundOpenStud_3062b from "../../assets/3D/RoundOpenStud_3062b/RoundOpenStud_3062b.glb";
 import dish_4_x_4_Round_Inverted_3960 from "../../assets/3D/Dish_4x4_Round_Inverted_3960/Dish_4x4_Round_Inverted_3960.glb";
+import train_Wheel_Spoked_2ac02 from "../../assets/3D/Train_Wheel_Spoked_2ac02/Train_Wheel_Spoked_2ac02.glb";
 
 export const phase12: PartInfo[] = [
   {
@@ -51,5 +52,33 @@ export const phase12: PartInfo[] = [
     partId: "3960",
     partPriority: 1,
     materialId: "blackPlasticMaterial",
+  },
+  {
+    partPath: train_Wheel_Spoked_2ac02,
+    partStartPosition: new Vector3(400, 0, -170),
+    partId: "2ac02",
+    partPriority: 1,
+    canNotBeRotated: true,
+  },
+  {
+    partPath: train_Wheel_Spoked_2ac02,
+    partStartPosition: new Vector3(440, 0, -170),
+    partId: "2ac02",
+    partPriority: 1,
+    canNotBeRotated: true,
+  },
+  {
+    partPath: train_Wheel_Spoked_2ac02,
+    partStartPosition: new Vector3(400, 0, -140),
+    partId: "2ac02",
+    partPriority: 1,
+    canNotBeRotated: true,
+  },
+  {
+    partPath: train_Wheel_Spoked_2ac02,
+    partStartPosition: new Vector3(440, 0, -140),
+    partId: "2ac02",
+    partPriority: 1,
+    canNotBeRotated: true,
   },
 ];
