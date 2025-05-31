e4b18f3adaadc49a74871d7c4acde4a51cc39699
Krzysztof Dziedzic <fanki11er@hotmail.com>
2025-05-31 22:23:47 +0200

    Added analytics with consent modal

---
+++ src/Components/Molecules/CookiesInformation/CookiesInformation.tsx
@@ -0,0 +1,74 @@
+import {
+  AcceptAllButton,
+  AcceptSelectedButton,
+  ButtonsWrapper,
+  ConsentsWrapper,
+  CookieImage,
+  CookiesInfoContent,
+  CookiesInfoDialog,
+  CookiesParagraph,
+  CookieTextWrapper,
+  RejectAllButton,
+} from "./CookiesInformation.styles";
+import cookieImage from "../../../assets/svg/Cookie.svg";
+import { useEffect, useRef } from "react";
+import ConsentCheckbox from "../ConsentCheckbox/ConsentCheckbox";
+import { updateConsent } from "../../../Utilities/analytics/analytics";
+import { Consent } from "../../../Types/generalTypes";
+
+const CookiesInformation = () => {
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
+  const analyticsInputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    dialogRef.current?.showModal();
+  }, []);
+
+  const handleUpdateConsent = (consent: Consent) => {
+    updateConsent(consent);
+
+    dialogRef.current?.close();
+  };
+
+  return (
+    <CookiesInfoDialog ref={dialogRef}>
+      <CookieImage src={cookieImage} alt={"Cookie"} />
+      <CookiesInfoContent>
+        <CookieTextWrapper>
+          <CookiesParagraph>
+            This website uses cookies to provide you with the best possible
+            experience. They also allow me to analyze users' behavior to improve
+            the website for you.
+          </CookiesParagraph>
+        </CookieTextWrapper>
+        <ButtonsWrapper>
+          <AcceptAllButton
+            onClick={() => handleUpdateConsent({ analytics: true })}
+          >
+            Accept All
+          </AcceptAllButton>
+          <AcceptSelectedButton
+            onClick={() =>
+              handleUpdateConsent({
+                analytics: analyticsInputRef.current?.checked || false,
+              })
+            }
+          >
+            Accept Selected
+          </AcceptSelectedButton>
+          <RejectAllButton
+            onClick={() => handleUpdateConsent({ analytics: false })}
+          >
+            Reject All
+          </RejectAllButton>
+        </ButtonsWrapper>
+        <ConsentsWrapper>
+          <ConsentCheckbox label={"Necessary"} checked disabled />
+          <ConsentCheckbox label={"Analytics"} ref={analyticsInputRef} />
+        </ConsentsWrapper>
+      </CookiesInfoContent>
+    </CookiesInfoDialog>
+  );
+};
+
+export default CookiesInformation;
