3c8a5942caa80699a8545e7a707ad351d2488bbc
Krzysztof Dziedzic <fanki11er@hotmail.com>
2025-08-16 09:16:18 +0200

    Added instruction textures for  set 4564

--- src/InstructionsTextures/Instruction_4564.ts
+++ src/InstructionsTextures/Instruction_4564.ts
@@ -1,10 +1,100 @@
 import { InstructionPageTextures } from "../Types/InstructionPageTextures";
 import instructionCover_01 from "../assets/Textures/Instruction_4564/Cover_01.jpg";
 import page_02 from "../assets/Textures/Instruction_4564/Page_02.jpg";
+import page_03 from "../assets/Textures/Instruction_4564/Page_03.jpg";
+import page_04 from "../assets/Textures/Instruction_4564/Page_04.jpg";
+import page_05 from "../assets/Textures/Instruction_4564/Page_05.jpg";
+import page_06 from "../assets/Textures/Instruction_4564/Page_06.jpg";
+import page_07 from "../assets/Textures/Instruction_4564/Page_07.jpg";
+import page_08 from "../assets/Textures/Instruction_4564/Page_08.jpg";
+import page_09 from "../assets/Textures/Instruction_4564/Page_09.jpg";
+import page_10 from "../assets/Textures/Instruction_4564/Page_10.jpg";
+import page_11 from "../assets/Textures/Instruction_4564/Page_11.jpg";
+import page_12 from "../assets/Textures/Instruction_4564/Page_12.jpg";
+import page_13 from "../assets/Textures/Instruction_4564/Page_13.jpg";
+import page_14 from "../assets/Textures/Instruction_4564/Page_14.jpg";
+import page_15 from "../assets/Textures/Instruction_4564/Page_15.jpg";
+import page_16 from "../assets/Textures/Instruction_4564/Page_16.jpg";
+import page_17 from "../assets/Textures/Instruction_4564/Page_17.jpg";
+import page_18 from "../assets/Textures/Instruction_4564/Page_18.jpg";
+import page_19 from "../assets/Textures/Instruction_4564/Page_19.jpg";
+import page_20 from "../assets/Textures/Instruction_4564/Page_20.jpg";
+import page_21 from "../assets/Textures/Instruction_4564/Page_21.jpg";
+import page_22 from "../assets/Textures/Instruction_4564/Page_22.jpg";
+import page_23 from "../assets/Textures/Instruction_4564/Page_23.jpg";
+import page_24 from "../assets/Textures/Instruction_4564/Page_24.jpg";
+import page_25 from "../assets/Textures/Instruction_4564/Page_25.jpg";
+import page_26 from "../assets/Textures/Instruction_4564/Page_26.jpg";
+import page_27 from "../assets/Textures/Instruction_4564/Page_27.jpg";
+import page_28 from "../assets/Textures/Instruction_4564/Page_28.jpg";
+import page_29 from "../assets/Textures/Instruction_4564/Page_29.jpg";
+import page_30 from "../assets/Textures/Instruction_4564/Page_30.jpg";
+import page_31 from "../assets/Textures/Instruction_4564/Page_31.jpg";
+import page_32 from "../assets/Textures/Instruction_4564/Page_32.jpg";
 
 export const instruction_textures_4564: InstructionPageTextures[] = [
   {
     frontPageTexturePath: instructionCover_01,
     backPageTexturePath: page_02,
+  },
+  {
+    frontPageTexturePath: page_03,
+    backPageTexturePath: page_04,
+  },
+  {
+    frontPageTexturePath: page_05,
+    backPageTexturePath: page_06,
+  },
+  {
+    frontPageTexturePath: page_07,
+    backPageTexturePath: page_08,
+  },
+  {
+    frontPageTexturePath: page_09,
+    backPageTexturePath: page_10,
+  },
+  {
+    frontPageTexturePath: page_11,
+    backPageTexturePath: page_12,
+  },
+  {
+    frontPageTexturePath: page_13,
+    backPageTexturePath: page_14,
+  },
+  {
+    frontPageTexturePath: page_15,
+    backPageTexturePath: page_16,
+  },
+  {
+    frontPageTexturePath: page_17,
+    backPageTexturePath: page_18,
+  },
+  {
+    frontPageTexturePath: page_19,
+    backPageTexturePath: page_20,
+  },
+  {
+    frontPageTexturePath: page_21,
+    backPageTexturePath: page_22,
+  },
+  {
+    frontPageTexturePath: page_23,
+    backPageTexturePath: page_24,
+  },
+  {
+    frontPageTexturePath: page_25,
+    backPageTexturePath: page_26,
+  },
+  {
+    frontPageTexturePath: page_27,
+    backPageTexturePath: page_28,
+  },
+  {
+    frontPageTexturePath: page_29,
+    backPageTexturePath: page_30,
+  },
+  {
+    frontPageTexturePath: page_31,
+    backPageTexturePath: page_32,
   },
 ];
