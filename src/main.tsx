751e3b1a1c33f52a1a02436c9f51a2991c8308d9
Krzysztof Dziedzic <fanki11er@hotmail.com>
2026-01-03 07:42:03 +0100

    Set new version to number to 26.01.03

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ const isProd = import.meta.env.PROD;
 
 export const withHelper = isProd ? false : true; // Disable context menu helper in production
 
-export const APP_VERSION = "25.10.19";
+export const APP_VERSION = "26.01.03";
 
 const queryClient = new QueryClient();
 
